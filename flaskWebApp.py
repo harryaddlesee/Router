c6f6b5dc7f0827e8323a08dadde1fa26571fc8b8
harryaddlesee <harry@addlesee.co.uk>
2021-12-08 14:13:33 +0000

    fixed python flask page

--- flaskWebApp.py
+++ flaskWebApp.py
@@ -1,14 +1,18 @@
-from flask import Flask
+from flask import Flask, render_template, url_for
 app = Flask(__name__)
 
 @app.route("/")
 @app.route("/home")
 def home():
-    return "<h1>Home Page</h1>"
+    return render_template('home.html')
+
+@app.route("/login")
+def login():
+    return render_template('login.html')
 
 @app.route("/about")
 def about():
-    return "<h1>About Page</h1>"
+    return render_template('about.html')
 
 if __name__ == '__main__':
     app.run(debug=True)
