408d0f132bb33306a4910248d66eb1f6ee711182
harryaddlesee <harry@addlesee.co.uk>
2021-12-08 17:54:31 +0000

    fixed spelling mistake

--- forms.py
+++ forms.py
@@ -6,7 +6,7 @@ class SignUpForm(FlaskForm):
     username = StringField('Username', validators=[DataRequired(), Length(min=2, max=20)])
     email = StringField('Email', validators=[DataRequired(), Email()])
     password = PasswordField('Password', validators=[DataRequired()])
-    confirm)password = PasswordField('Confirm password', validators=[DataRequired(), EqualTo('password')])
+    confirm_password = PasswordField('Confirm password', validators=[DataRequired(), EqualTo('password')])
     submit = SubmitField('Sign Up')
 
 class LoginForm(FlaskForm):
