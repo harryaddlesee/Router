2c9aa88a4b262d1caf62cc7fb57a6aad0fb6211c
harryaddlesee <harry@addlesee.co.uk>
2021-12-09 14:34:28 +0000

    fixed spelling

--- flaskWebApp.py
+++ flaskWebApp.py
@@ -10,9 +10,9 @@ db = SQLAlchemy(app)
 
 class User(db.Model):
     id = db.Column(db.Integer, primary_key=True)
-    username = db.Column(db.String(20), unique=True, nullanle=False)
-    email = db.Column(db.String(120), unique=True, nullanle=False)
-    image_file = db.Column(db.String(20), nullanle=False, default='default.jpg')
+    username = db.Column(db.String(20), unique=True, nullable=False)
+    email = db.Column(db.String(120), unique=True, nullable=False)
+    image_file = db.Column(db.String(20), nullable=False, default='default.jpg')
     password = db.Column(db.String(60), nullable=False)
     posts = db.relationship('Post', backref='author', lazy=True)
 
@@ -22,7 +22,7 @@ class User(db.Model):
 
 class Post(db.Model):
     id = db.Column(db.Integer, primary_key=True)
-    title = db.Column(db.String(100), nullanle=False)
+    title = db.Column(db.String(100), nullable=False)
     date_posted = db.Colum(db.DateTime, nullable=False, default=datetime.utcnow)
     content = db.Column(db.Text, nullable=False)
     user_id = db.Column(db.Integer, db.ForeignKey('user.id'), nullable=False)
