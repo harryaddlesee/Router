4f00386f2f81491d531560670b928924c65e0b17
harryaddlesee <harry@addlesee.co.uk>
2021-12-09 14:29:45 +0000

    fixed sqlalchemy

--- flaskWebApp.py
+++ flaskWebApp.py
@@ -1,6 +1,6 @@
 from datetime import datetime
 from flask import Flask, render_template, url_for, flash, redirect
-from flask_sqlalchemy import SQLALCHEMY
+from flask_sqlalchemy import SQLAlchemy
 from forms import SignUpForm, LoginForm
 
 app = Flask(__name__)
